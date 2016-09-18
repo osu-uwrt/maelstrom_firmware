ddf3e57cc415f8ea112ead491884fedfa62dc180
charles fletcher <fletcher.236@osu.edu>
2016-09-18 15:31:48 -0400

    added up to date firmware

--- firmware/thruster_controller/thruster_controller.ino
+++ firmware/thruster_controller/thruster_controller.ino
@@ -2,6 +2,7 @@
 #include <ros.h>
 #include <riptide_msgs/PwmStamped.h>
 #include <std_msgs/Int8.h>
+#include <std_msgs/Empty.h>
 
 // Thrusters off!
 int STOP = 1500;
@@ -10,6 +11,9 @@ int ONE_BYTE = 1;
 // Addresses
 int ESC_BOARD[] = {1, 2, 4, 8, 16};
 
+int timeout=0;
+int kill_status=0;
+
 // Function prototypes
 int16_t valid(int16_t pwm);
 void callback(const riptide_msgs::PwmStamped &cmd);
@@ -17,7 +21,9 @@ void callback(const riptide_msgs::PwmStamped &cmd);
 // ROS is the best
 ros::NodeHandle nh;
 std_msgs::Int8 state;
+std_msgs::Empty kill;
 ros::Publisher state_pub("state/esc", &state);
+ros::Publisher kill_pub("state/kill", &kill);
 ros::Subscriber<riptide_msgs::PwmStamped> cmd_sub("command/pwm", &callback);
 
 void setup()
@@ -27,8 +33,9 @@ void setup()
   nh.initNode();
   nh.subscribe(cmd_sub);
   nh.advertise(state_pub);
+  nh.advertise(kill_pub);
 
-  // Surge LEDs
+ // Surge LEDs
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
@@ -43,6 +50,8 @@ void setup()
   // Sway LEDs
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
+  
+  pinMode(13, INPUT);
 }
 
 void loop()
@@ -51,14 +60,72 @@ void loop()
   nh.spinOnce();
 
   delay(5);
+  kill_status = 0;
+      if (!digitalRead(13))
+    {
+      kill_status=1;
+    }
+    else if (kill_status)
+    {
+      delay(5000);
+      kill_status=0;
+    }
+    else
+    {
+      kill_pub.publish(&kill);
+    }
+  
+  if ((timeout>=50)||kill_status){
+    Wire.beginTransmission(ESC_BOARD[0]);
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.endTransmission();
+
+    Wire.beginTransmission(ESC_BOARD[1]);
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.endTransmission();
+
+    Wire.beginTransmission(ESC_BOARD[2]);
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.endTransmission();
+
+    Wire.beginTransmission(ESC_BOARD[3]);
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.endTransmission();
+
+    Wire.beginTransmission(ESC_BOARD[4]);
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.write(valid(STOP) >> 8);
+    Wire.write(valid(STOP));
+    Wire.endTransmission();
+    
+    
+
+  }
+  timeout++;
+
+  
+
 }
+
 
 // Production ready function
 void callback(const riptide_msgs::PwmStamped &cmd)
 {
   //
-  // Write PWM data & request remote checksums
-  //
+ //
 
   Wire.beginTransmission(ESC_BOARD[0]);
   Wire.write(valid(cmd.pwm.surge_port_hi) >> 8);
@@ -128,12 +195,17 @@ void callback(const riptide_msgs::PwmStamped &cmd)
   else {digitalWrite(10, LOW); }
   if(cmd.pwm.sway_aft != STOP) { digitalWrite(11, HIGH); }
   else {digitalWrite(11, LOW); }
+  
+  timeout=0;
+  
 }
 
 // Ensure 1100 <= pwm <= 1900
 int16_t valid(int16_t pwm)
 {
-  pwm = pwm > 1900 ? 1900 : pwm;
+    pwm = pwm > 1900 ? 1900 : pwm;
   pwm = pwm < 1100 ? 1100 : pwm;
   return pwm;
 }
+
+
