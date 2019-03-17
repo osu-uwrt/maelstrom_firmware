9e9cc0d0356a7ea12e1b029391c97d7c57230afb
Blaine Miller <blaine141@gmail.com>
2019-03-16 21:59:16 -0700

    Cleaning out website

--- Copro/operator_console/website/src/app/@theme/layouts/sample/sample.layout.ts
+++ Copro/operator_console/website/src/app/@theme/layouts/sample/sample.layout.ts
@@ -21,17 +21,7 @@ import { StateService } from '../../../@core/utils';
         <ngx-header [position]="sidebar.id === 'start' ? 'normal': 'inverse'"></ngx-header>
       </nb-layout-header>
 
-      <nb-sidebar class="menu-sidebar"
-                   tag="menu-sidebar"
-                   responsive
-                   [end]="sidebar.id === 'end'">
-        <nb-sidebar-header *ngIf="currentTheme !== 'corporate'">
-          <a href="#" class="btn btn-hero-success main-btn">
-            <i class="ion ion-social-github"></i> <span>Support Us</span>
-          </a>
-        </nb-sidebar-header>
-        <ng-content select="nb-menu"></ng-content>
-      </nb-sidebar>
+
 
       <nb-layout-column class="main-content">
         <ng-content select="router-outlet"></ng-content>
