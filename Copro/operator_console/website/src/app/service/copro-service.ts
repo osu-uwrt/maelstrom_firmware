e77c9cccc46a5fb224e1f12a73bf2953af28e939
Andrew Tian <tian.525@osu.edu>
2019-04-27 15:08:37 -0400

    changed mobopower and thrusterpower to 12V and 5V

--- Copro/operator_console/website/src/app/service/copro-service.ts
+++ Copro/operator_console/website/src/app/service/copro-service.ts
@@ -10,71 +10,79 @@ const COPRO_URL = "http://localhost:2000";
 
 @Injectable()
 export class CoproService {
-  coproDisconnected = new EventEmitter();
-  connected = true;
+	coproDisconnected = new EventEmitter();
+	connected = true;
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) { }
 
-  setMoboPower(state: boolean): Observable<any> {
-    return this.command(0, state ? 1 : 0);
-  }
+	setMoboPower(state: boolean): Observable<any> {
+		return this.command(0, state ? 1 : 0);
+	}
 
-  setJetsonPower(state: boolean): Observable<any> {
-    return this.command(1, state ? 1 : 0);
-  }
+	setJetsonPower(state: boolean): Observable<any> {
+		return this.command(1, state ? 1 : 0);
+	}
 
-  setThrusterPower(state: boolean): Observable<any> {
-    return this.command(2, state ? 1 : 0);
-  }
+	setThrusterPower(state: boolean): Observable<any> {
+		return this.command(2, state ? 1 : 0);
+	}
 
-  setPeltierPower(state: boolean): Observable<any> {
-    return this.command(3, state ? 1 : 0);
-  }
+	setPeltierPower(state: boolean): Observable<any> {
+		return this.command(3, state ? 1 : 0);
+	}
 
-  getBatVoltages(): Observable<number[]> {
-    return this.command(4).pipe(
-      map(x => {
-        return [(x[0] * 256 + x[1]) / 100.0, (x[2] * 256 + x[3]) / 100.0];
-      })
-    );
-  }
+	getBatVoltages(): Observable<number[]> {
+		return this.command(4).pipe(
+			map(x => {
+				return [(x[0] * 256 + x[1]) / 100.0, (x[2] * 256 + x[3]) / 100.0];
+			})
+		);
+	}
 
-  getBatCurrents(): Observable<number[]> {
-    return this.command(5).pipe(
-      map(x => {
-        return [(x[0] * 256 + x[1]) / 100.0, (x[2] * 256 + x[3]) / 100.0];
-      })
-    );
-  }
+	getBatCurrents(): Observable<number[]> {
+		return this.command(5).pipe(
+			map(x => {
+				return [(x[0] * 256 + x[1]) / 100.0, (x[2] * 256 + x[3]) / 100.0];
+			})
+		);
+	}
 
-  getLogicCurrents(): Observable<number[]> {
-	  return this.command(8).pipe(
-		  map(x => {
-			  return [(x[0] * 256 + x[1]) / 1000.0, (x[2] * 256 + x[3]) / 1000.0, (x[4] * 256 + x[5]) / 1000.0];
-		  })
-	  )
-  }
+	getLogicCurrents(): Observable<number[]> {
+		return this.command(8).pipe(
+			map(x => {
+				return [(x[0] * 256 + x[1]) / 1000.0, (x[2] * 256 + x[3]) / 1000.0, (x[4] * 256 + x[5]) / 1000.0];
+			})
+		)
+	}
 
-  getTemperature(): Observable<number> {
-    return this.command(6).pipe(map(x => (x[0] * 256 + x[1]) / 10.0));
-  }
+	getTemperature(): Observable<number> {
+		return this.command(6).pipe(map(x => (x[0] * 256 + x[1]) / 10.0));
+	}
 
-  getDepth(): Observable<number> {
-	  return this.command(11).pipe(
-		  map(x=> (x[0]*65536+x[1]*256+x[2])/100000.0)
-	  );
-  }
+	getDepth(): Observable<number> {
+		return this.command(11).pipe(
+			map(x => (x[0] * 65536 + x[1] * 256 + x[2]) / 100000.0)
+		);
+	}
 
-  private command(...args: number[]): Observable<number[]> {
-    return this.http.post<number[]>(COPRO_URL, [...args]).pipe(
-      catchError(err => {
-        if (this.connected && err.status === 503) {
-          this.connected = false;
-          this.coproDisconnected.emit();
-        }
-        return throwError(err);
-      }),
-      tap(() => (this.connected = true))
-    );
-  }
+	setTwelvePower(state: boolean): Observable<any> {
+		return this.command(12, state ? 1 : 0);
+	}
+
+	setFivePower(state: boolean): Observable<any> {
+		return this.command(13, state ? 1 : 0);
+	}
+
+	private command(...args: number[]): Observable<number[]> {
+		return this.http.post<number[]>(COPRO_URL, [...args]).pipe(
+			catchError(err => {
+				if (this.connected && err.status === 503) {
+					this.connected = false;
+					this.coproDisconnected.emit();
+				}
+				return throwError(err);
+			}),
+			tap(() => (this.connected = true))
+		);
+	}
 }
